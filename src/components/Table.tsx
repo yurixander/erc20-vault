5fe0c8867b0de93396b880c3da090ab921d1fd6a
Cristian Carralero <carralerocristianX@gmail.com>
2024-11-24 08:01:54 -0500

    fix: Fix tailwindcss shorthand

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -94,7 +94,7 @@ const TableCell = React.forwardRef<
   <td
     ref={ref}
     className={cn(
-      "p-2 align-middle [&:has([role=checkbox])]:pr-0 [&>[role=checkbox]]:translate-y-[2px]",
+      "p-2 align-middle [&:has([role=checkbox])]:pr-0 [&>[role=checkbox]]:translate-y-0.5",
       className
     )}
     {...props}
