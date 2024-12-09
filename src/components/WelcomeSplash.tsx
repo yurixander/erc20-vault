02cd62789b7b885d269a66d81ba22832725434b6
lazaroysr96 <156694602+lazaroysr96@users.noreply.github.com>
2024-12-09 08:00:47 -0500

    feat: implement functions in buttons

--- src/components/WelcomeSplash.tsx
+++ src/components/WelcomeSplash.tsx
@@ -1,12 +1,13 @@
-import { FC } from "react"
+import { FC } from "react";
 import {
   IoIosHelpBuoy,
   IoIosStar,
   IoIosWallet,
   IoLogoGithub,
 } from "react-icons/io";
-import { Heading, Text } from "./Typography"
-import { IconType } from "react-icons/lib"
+import { Heading, Text } from "./Typography";
+import { IconType } from "react-icons/lib";
+import { ConnectButton as RainbowKitConnectButton } from "@rainbow-me/rainbowkit";
 
 const WelcomeSplash: FC = () => {
   return (
@@ -23,19 +24,33 @@ const WelcomeSplash: FC = () => {
         </Text>
       </div>
       <div className="flex gap-4 w-full p-4 items-center justify-center">
-        <CardButton Icon={IoIosWallet} label="Connect You Wallet" />
-        <CardButton Icon={IoLogoGithub} label="GitHub" />
-        <CardButton Icon={IoIosHelpBuoy} label="Help" />
+        <CustomConnectButton />
+        <CardButton
+          onClick={() => {
+            open("https://github.com/yurixander/erc20-vault");
+          }}
+          Icon={IoLogoGithub}
+          label="GitHub"
+        />
+        {/**TODO: Create and implement Help page */}
+        <CardButton onClick={() => {}} Icon={IoIosHelpBuoy} label="Help" />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default WelcomeSplash
+export default WelcomeSplash;
 
-const CardButton: FC<{ Icon: IconType; label: string }> = ({ Icon, label }) => {
+const CardButton: FC<{
+  Icon: IconType;
+  label: string;
+  onClick: () => void;
+}> = ({ Icon, label, onClick }) => {
   return (
-    <button className="flex flex-col bg-blue-600 border shadow size-40 rounded-lg items-center justify-center hover:scale-105">
+    <button
+      onClick={onClick}
+      className="flex flex-col bg-blue-600 border shadow size-40 rounded-lg items-center justify-center hover:scale-105"
+    >
       <div>
         <Icon size={48} className="text-white" />
       </div>
@@ -43,5 +58,35 @@ const CardButton: FC<{ Icon: IconType; label: string }> = ({ Icon, label }) => {
         {label}
       </Text>
     </button>
-  )
-}
+  );
+};
+
+const CustomConnectButton: FC = () => {
+  return (
+    <RainbowKitConnectButton.Custom>
+      {({ account, chain, openConnectModal, mounted }) => {
+        const connected =
+          mounted && (chain !== undefined || account !== undefined);
+
+        if (!connected) {
+          return (
+            <button
+              onClick={openConnectModal}
+              className="flex flex-col bg-blue-600 border shadow size-40 rounded-lg items-center justify-center hover:scale-105"
+            >
+              <div>
+                <IoIosWallet size={48} className="text-white" />
+              </div>
+              <Text
+                size="5"
+                className="flex justify-center text-center text-white"
+              >
+                Connect You Wallet
+              </Text>
+            </button>
+          );
+        }
+      }}
+    </RainbowKitConnectButton.Custom>
+  );
+};
