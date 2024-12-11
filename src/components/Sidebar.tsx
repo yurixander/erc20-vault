2f61b7227220b5dea541fb4ae4d1b6e7d74b081f
Cristian Carralero <carralerocristianX@gmail.com>
2024-12-11 11:13:05 -0500

    feat: Implement tvl

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,5 +1,5 @@
 import { cn } from "@/lib/utils";
-import { FC } from "react";
+import { FC, useCallback } from "react";
 import { IoIosInformationCircle } from "react-icons/io";
 import { Heading, Text } from "./Typography";
 import {
@@ -8,10 +8,75 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from "@/components/Tooltip";
+import useContractReadOnce from "@/hooks/useContractRead";
+import VAULT_ABI from "@/abi/vaultAbi";
+import { VAULT_CONTRACT_ADDRESS } from "@/config/constants";
+import BN from "bn.js";
+import { Erc20TokenDefinition } from "@/config/types";
+import useTokenPrice from "@/hooks/useTokenPrice";
+import { getTokenByAddress } from "@/utils/findTokenByAddress";
+import { convertBNToAmount } from "@/utils/amount";
+import Decimal from "decimal.js";
 
 const TVL_BALANCE = "$345.15M";
 
 const AppSidebar: FC<{ className?: string }> = ({ className }) => {
+  const readOnce = useContractReadOnce(VAULT_ABI);
+  const { getAllPricesInUsd, getPriceInUsd } = useTokenPrice();
+
+  const fetchTvl = useCallback(async () => {
+    const rawDepositors = await readOnce({
+      address: VAULT_CONTRACT_ADDRESS,
+      functionName: "getDepositors",
+      args: [],
+    });
+
+    if (rawDepositors instanceof Error) {
+      throw rawDepositors;
+    }
+
+    const prices = await getAllPricesInUsd();
+
+    if (prices === null) {
+      throw new Error("No prices available.");
+    }
+
+    const tvl = new BN(0);
+
+    for (const depositor of rawDepositors) {
+      const rawDeposit = await readOnce({
+        address: VAULT_CONTRACT_ADDRESS,
+        functionName: "getDeposits",
+        args: [depositor],
+      });
+
+      if (rawDeposit instanceof Error) {
+        throw new Error(`Error getting deposit for ${depositor}`);
+      }
+
+      for (const deposit of rawDeposit) {
+        if (deposit.withdrawn) {
+          continue;
+        }
+
+        const tokenDef = getTokenByAddress(deposit.tokenAddress);
+
+        const priceOfToken =
+          prices[tokenDef.id] ?? (await getPriceInUsd(tokenDef.id));
+
+        const depositPrice = priceOfDeposit(
+          deposit.amount,
+          priceOfToken,
+          tokenDef,
+        );
+
+        tvl.add(depositPrice);
+      }
+    }
+
+    return tvl;
+  }, [readOnce, getAllPricesInUsd, getPriceInUsd]);
+
   return (
     <div className={cn("flex flex-col", className)}>
       <header className="flex h-16 w-full items-center justify-between px-4 sm:h-20 sm:border-b sm:border-b-blue-200/50 md:h-24 dark:sm:border-b-gray-200/50">
@@ -48,5 +113,17 @@ const AppSidebar: FC<{ className?: string }> = ({ className }) => {
     </div>
   );
 };
+
+function priceOfDeposit(
+  rawAmount: bigint,
+  priceOfToken: number,
+  { decimals }: Erc20TokenDefinition,
+): BN {
+  const amount = convertBNToAmount(new BN(rawAmount.toString()), decimals);
+  const result = new Decimal(amount);
+
+  result.mul(priceOfToken);
+  return new BN(result.toFixed(0));
+}
 
 export default AppSidebar;
