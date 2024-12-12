dccca5ebed60d656adbb2553c784a55a42c2cd79
Cristian Carralero <carralerocristianX@gmail.com>
2024-12-12 10:29:31 -0500

    fix: Clean dev preview

--- src/containers/DevPreview.tsx
+++ src/containers/DevPreview.tsx
@@ -1,14 +1,7 @@
-import DisplayTvl from "@/components/DisplayTvl";
 import { FC } from "react";
 
 const DevPreview: FC = () => {
-  return (
-    <>
-      <div className="p-10 bg-blue-600">
-        <DisplayTvl />
-      </div>
-    </>
-  );
+  return <></>;
 };
 
 export default DevPreview;
