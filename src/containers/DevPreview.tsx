e85c14d120dc526a8934fe67bcdbcc1462865076
Cristian Carralero <carralerocristianX@gmail.com>
2024-12-14 16:00:22 -0500

    fix: Clean dev preview

--- src/containers/DevPreview.tsx
+++ src/containers/DevPreview.tsx
@@ -1,37 +1,7 @@
-import { Erc20TokenId, ERC20TokenPrices } from "@/config/types";
-import useTokenPrice from "@/hooks/useTokenPrice";
-import { FC, useEffect, useState } from "react";
+import { FC } from "react";
 
 const DevPreview: FC = () => {
-  const [prices, setPrices] = useState<ERC20TokenPrices | null>(null);
-
-  const { getAllPrices } = useTokenPrice(
-    Object.values(Erc20TokenId).filter((e) => e !== Erc20TokenId.MTK),
-  );
-
-  useEffect(() => {
-    if (getAllPrices === null) {
-      return;
-    }
-
-    getAllPrices().then(setPrices);
-  }, [getAllPrices]);
-
-  return (
-    <>
-      {prices === null ? (
-        <span>Null Prices</span>
-      ) : (
-        <div className="flex flex-col gap-4">
-          {Object.entries(prices).map((price) => (
-            <span key={price[0]}>
-              {price[0]}: {price[1]}
-            </span>
-          ))}
-        </div>
-      )}
-    </>
-  );
+  return <></>;
 };
 
 export default DevPreview;
