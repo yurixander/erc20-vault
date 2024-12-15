93436541eb7bb03cf76fc22e55c999cf109a1626
lazaroysr96 <156694602+lazaroysr96@users.noreply.github.com>
2024-12-15 09:39:25 -0500

    feat: update buttonSwitchTheme

--- src/components/ButtonSwitchTheme.tsx
+++ src/components/ButtonSwitchTheme.tsx
@@ -6,13 +6,14 @@ import useTheme from "@/hooks/useTheme";
 import { Icon } from "@radix-ui/react-select";
 import { IoIosMoon, IoIosSunny } from "react-icons/io";
 
-const ButtonSwitchTheme: FC = () =>{
-    const {toggleTheme, theme} = useTheme()
-    return (
-        <Button onClick={toggleTheme} className="size-8">
-          <Icon>{theme === "dark" ? (<IoIosSunny/>) : (<IoIosMoon/>)}</Icon>
-        </Button>
-    )
-}
+const ButtonSwitchTheme: FC = () => {
+  const { toggleTheme, theme } = useTheme();
 
-export default ButtonSwitchTheme
+  return (
+    <Button onClick={toggleTheme} className="size-8">
+      <Icon>{theme === "dark" ? <IoIosSunny /> : <IoIosMoon />}</Icon>
+    </Button>
+  );
+};
+
+export default ButtonSwitchTheme;
