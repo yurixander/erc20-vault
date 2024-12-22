21466112dcbb3b2e7413093a9c4c5e7f5c91996d
lazaroysr96 <156694602+lazaroysr96@users.noreply.github.com>
2024-12-22 10:29:55 -0500

    feat: remove useThemeReturnType

--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -1,30 +1,24 @@
-import {useCallback, useEffect, useState} from "react"
+import { useCallback, useEffect, useState } from "react";
 
-type UseThemeReturnType = {
-  toggleTheme: () => void
-  theme: string
-}
+export const THEME_KEY = "theme_mode";
 
-export const THEME_KEY = "theme_mode"
+const DARK_MODE = "dark";
+const LIGHT_MODE = "light";
+const savedTheme = localStorage.getItem(THEME_KEY);
 
-const DARK_MODE = "dark"
-const LIGHT_MODE = "light"
-const savedTheme = localStorage.getItem(THEME_KEY)
-
-const useTheme = (): UseThemeReturnType => {
-
-  const [theme, setTheme] = useState(savedTheme ?? LIGHT_MODE)
+const useTheme = () => {
+  const [theme, setTheme] = useState(savedTheme ?? LIGHT_MODE);
 
   useEffect(() => {
-    document.documentElement.className = theme
-    localStorage.setItem(THEME_KEY, theme)
-  }, [theme])
+    document.documentElement.className = theme;
+    localStorage.setItem(THEME_KEY, theme);
+  }, [theme]);
 
   const toggleTheme = useCallback(() => {
-    setTheme(theme === DARK_MODE ? LIGHT_MODE : DARK_MODE)
-  }, [theme])
+    setTheme(theme === DARK_MODE ? LIGHT_MODE : DARK_MODE);
+  }, [theme]);
 
-  return {toggleTheme, theme}
-}
+  return { toggleTheme, theme };
+};
 
-export default useTheme
+export default useTheme;
