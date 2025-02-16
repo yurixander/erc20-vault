14b7ce85512cd5dc5a60ed0a9d55ce83c22f54e8
Ariel11R <arielrodriguezfuentes15@gmail.com>
2025-02-16 11:23:48 +0100

    Create AppLinks enum for internal links

---
+++ src/utils/AppLinks.ts
@@ -0,0 +1,9 @@
+export enum Links {
+  GitHub = "https://github.com/yurixander/erc20-vault",
+  Home = "/",
+  About = "/about",
+  Contact = "/contact",
+  Dashboard = "/dashboard",
+  Profile = "/profile",
+  Settings = "/settings",
+}
