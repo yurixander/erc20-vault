d28d0eee795f98c5f7ebdf07cf42a94bf754c28f
Ariel11R <arielrodriguezfuentes15@gmail.com>
2025-02-16 09:21:18 +0100

    Update Sidebar.tsx

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -9,10 +9,8 @@ const AppSidebar: FC<{ className?: string }> = ({ className }) => {
     <div className={cn("flex flex-col", className)}>
       <header className="flex h-16 w-full items-center justify-between px-4 sm:h-20 sm:border-b sm:border-b-blue-200/50 md:h-24 dark:sm:border-b-gray-200/50">
         <div className="size-12 rounded-full bg-blue-100" />
-
         <div className="h-8 w-24 rounded-md bg-blue-100" />
       </header>
-
       <div className="grow px-4 pt-4">
         <DisplayTvl />
       </div>
