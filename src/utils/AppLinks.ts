d71be48f0f33292cde63458cef1595757285a5e6
Ariel11R <arielrodriguezfuentes15@gmail.com>
2025-02-16 19:02:11 +0100

    Update AppLinks.ts

--- src/utils/AppLinks.ts
+++ src/utils/AppLinks.ts
@@ -1,9 +1,3 @@
 export enum Links {
   GitHub = "https://github.com/yurixander/erc20-vault",
-  Home = "/",
-  About = "/about",
-  Contact = "/contact",
-  Dashboard = "/dashboard",
-  Profile = "/profile",
-  Settings = "/settings",
 }
